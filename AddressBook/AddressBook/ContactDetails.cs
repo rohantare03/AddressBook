3fdf9093fb7c1d716a43368176f75be86a80cb54
rohantare03 <rohatare123@gmail.com>
2022-05-20 20:41:19 +0530

    [Rohan] Add : Creating a Contact List

--- AddressBook/AddressBook/ContactDetails.cs
+++ AddressBook/AddressBook/ContactDetails.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    internal class ContactDetails 
+    internal class ContactDetails
     {
         public string FirstName;
         public string LastName;
@@ -17,5 +17,51 @@ namespace AddressBook
         public long PhoneNumber;
         public string Email;
 
+        public void Contact()
+        {
+            List<ContactDetails> list = new List<ContactDetails>();
+            ContactDetails contact = new ContactDetails();
+
+            Console.WriteLine("Enter the First Name:");
+            contact.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Last Name:");
+            contact.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Address:");
+            contact.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter the City:");
+            contact.City = Console.ReadLine();
+
+            Console.WriteLine("Enter the State:");
+            contact.State = Console.ReadLine();
+
+            Console.WriteLine("Enter the Zipcode:");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the phone number:");
+            contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter the Email:");
+            contact.Email = Console.ReadLine();
+
+
+            list.Add(contact);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("FirstName: " + contact.FirstName);
+            Console.WriteLine("LastName: " + contact.LastName);
+            Console.WriteLine("Address: " + contact.Address);
+            Console.WriteLine("City: " + contact.City);
+            Console.WriteLine("State: " + contact.State);
+            Console.WriteLine("ZipCode: " + contact.Zip);
+            Console.WriteLine("Phone Number: " + contact.PhoneNumber);
+            Console.WriteLine("Email: " + contact.Email);
+        }
     }
+
 }
+
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -5,47 +5,8 @@ namespace AddressBook
     {
         public static void Main(string[] args)
         {
-            List<ContactDetails> list = new List<ContactDetails>();
-            ContactDetails contact = new ContactDetails();
-
-            Console.WriteLine("Enter the First Name:");
-            contact.FirstName = Console.ReadLine();
-
-            Console.WriteLine("Enter the Last Name:");
-            contact.LastName = Console.ReadLine();
-
-            Console.WriteLine("Enter the Address:");
-            contact.Address = Console.ReadLine();
-
-            Console.WriteLine("Enter the City:");
-            contact.City = Console.ReadLine();
-
-            Console.WriteLine("Enter the State:");
-            contact.State = Console.ReadLine();
-
-            Console.WriteLine("Enter the Zipcode:");
-            contact.Zip = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the phone number:");
-            contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
-
-            Console.WriteLine("Enter the Email:");
-            contact.Email = Console.ReadLine();
-
-
-            list.Add(contact);
-
-            Console.WriteLine("\n");
-            Console.WriteLine("\n");
-
-            Console.WriteLine("FirstName: " + contact.FirstName);
-            Console.WriteLine("LastName: " + contact.LastName);
-            Console.WriteLine("Address: " + contact.Address);
-            Console.WriteLine("City: " + contact.City);
-            Console.WriteLine("State: " + contact.State);
-            Console.WriteLine("ZipCode: " + contact.Zip);
-            Console.WriteLine("Phone Number: " + contact.PhoneNumber);
-            Console.WriteLine("Email: " + contact.Email);
+            ContactDetails contactDetails = new ContactDetails();
+            contactDetails.Contact();
         }
 
     }
