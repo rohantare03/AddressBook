843f09ec429d1f36e76c842b80e59ce6efb4150c
rohantare03 <rohatare123@gmail.com>
2022-05-20 16:06:01 +0530

    [Rohan] Add : Creating a Contact List

---
+++ AddressBook/AddressBook/ContactDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class ContactDetails 
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public int Zip;
+        public long PhoneNumber;
+        public string Email;
+
+    }
+}
