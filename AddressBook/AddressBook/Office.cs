0ffd149fbb873949742e9a254e7df6fb7c3073f0
rohantare03 <rohatare123@gmail.com>
2022-05-24 21:49:15 +0530

    [Rohan] Add : Creating Multiple Address Book

---
+++ AddressBook/AddressBook/Office.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Office
+    {
+        ArrayList contact = new ArrayList();
+        public void AddingContacts()
+        {
+            Console.WriteLine("Enter number of Contacts:");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i <= Num; i++)
+            {
+                Console.WriteLine("Add Details of the person: " + i);
+
+                Console.WriteLine("Enter the First Name:");
+                string FirstName = Console.ReadLine();
+
+                Console.WriteLine("Enter the Last Name:");
+                string LastName = Console.ReadLine();
+
+                Console.WriteLine("Enter the Address:");
+                string Address = Console.ReadLine();
+
+                Console.WriteLine("Enter the City:");
+                string City = Console.ReadLine();
+
+                Console.WriteLine("Enter the State:");
+                string State = Console.ReadLine();
+
+                Console.WriteLine("Enter the Email:");
+                string Email = Console.ReadLine();
+
+                Console.WriteLine("Enter the Zipcode:");
+                string Zip = Console.ReadLine();
+
+                Console.WriteLine("Enter the phone number:");
+                string PhoneNumber = Console.ReadLine();
+
+                contact.Add(FirstName);
+                contact.Add(LastName);
+                contact.Add(Address);
+                contact.Add(City);
+                contact.Add(State);
+                contact.Add(Email);
+                contact.Add(Zip);
+                contact.Add(PhoneNumber);
+            }
+        }
+        public void OfficeView()
+        {
+
+            int j = 0;
+            int count = 1;
+            foreach (var item in contact)
+            {
+                if (j % 8 == 0)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine("\n");
+
+                    Console.WriteLine("\nNumber of person :" + count);
+                    Console.WriteLine("FirstName: " + contact[j]);
+                    Console.WriteLine("LastName: " + contact[j + 1]);
+                    Console.WriteLine("Address : " + contact[j + 2]);
+                    Console.WriteLine("City: " + contact[j + 3]);
+                    Console.WriteLine("State: " + contact[j + 4]);
+                    Console.WriteLine("Email: " + contact[j + 5]);
+                    Console.WriteLine("ZipCode: " + contact[j + 6]);
+                    Console.WriteLine("Phone Number: " + contact[j + 7]);
+                    count++;
+                }
+                j++;
+            }
+
+        }
+
+
+        public void EditContact()
+        {
+            Console.WriteLine("\nEnter the Name whose data you want to change");
+            string Firstname = Console.ReadLine();
+            for (int i = 0; i < contact.Count; i += 8)
+            {
+                if (String.Equals(Firstname, (contact[i])))
+                {
+                    Console.WriteLine("Name:" + contact + "==" + contact[i]);
+                    Console.WriteLine("Press 1: for first name");
+                    Console.WriteLine("Press 2: for last name name");
+                    Console.WriteLine("Press 3: for Address");
+                    Console.WriteLine("Press 4: for city name");
+                    Console.WriteLine("Press 5: for state name");
+                    Console.WriteLine("Press 6: for Email ID");
+                    Console.WriteLine("Press 7: for ZIP number");
+                    Console.WriteLine("Press 8: for phone number");
+
+                    int check = Convert.ToInt32(Console.ReadLine());
+                    switch (check)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter your correct first name");
+                            string FirstName = Console.ReadLine();
+                            contact[i] = FirstName;
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter your correct last name");
+                            string LastName = Console.ReadLine();
+                            contact[i + 1] = LastName;
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter your correct Address");
+                            string Address = Console.ReadLine();
+                            contact[i + 2] = Address;
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter your correct city name");
+                            string City = Console.ReadLine();
+                            contact[i + 3] = City;
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter your correct state name");
+                            string State = Console.ReadLine();
+                            contact[i + 4] = State;
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter your correct EmailID");
+                            string EmailID = Console.ReadLine();
+                            contact[i + 5] = EmailID;
+                            break;
+                        case 7:
+                            Console.WriteLine("Enter your correct ZIP number");
+                            string ZIP = Console.ReadLine();
+                            contact[i + 6] = ZIP;
+                            break;
+                        case 8:
+                            Console.WriteLine("Enter your correct Phone number");
+                            string PhoneNumber = Console.ReadLine();
+                            contact[i + 7] = PhoneNumber;
+                            break;
+
+                        default:
+                            Console.WriteLine("Please enter a valid input");
+                            break;
+
+                    }
+                }
+            }
+            int j = 0;
+            int count = 1;
+            foreach (var item in contact)
+            {
+                if (j % 8 == 0)
+                {
+                    Console.WriteLine("\nNumber of person :" + count);
+                    Console.WriteLine("FirstName: " + contact[j]);
+                    Console.WriteLine("LastName: " + contact[j + 1]);
+                    Console.WriteLine("Address : " + contact[j + 2]);
+                    Console.WriteLine("City: " + contact[j + 3]);
+                    Console.WriteLine("State: " + contact[j + 4]);
+                    Console.WriteLine("Email: " + contact[j + 5]);
+                    Console.WriteLine("ZipCode: " + contact[j + 6]);
+                    Console.WriteLine("Phone Number: " + contact[j + 7]);
+                    count++;
+
+                }
+                j++;
+            }
+
+        }
+        public void DeleteContact()
+        {
+            Console.WriteLine("Enter the name you want to remove");
+            string Firstname = Console.ReadLine();
+            for (int i = 0; i < contact.Count; i += 8)
+            {
+                if (string.Equals(Firstname, (contact[i])))
+                {
+                    contact.RemoveAt(i);
+                    contact.RemoveAt(i);
+                    contact.RemoveAt(i);
+                    contact.RemoveAt(i);
+                    contact.RemoveAt(i);
+                    contact.RemoveAt(i);
+                    contact.RemoveAt(i);
+                    contact.RemoveAt(i);
+                }
+            }
+            int j = 0;
+            int count = 1;
+            foreach (var item in contact)
+            {
+                if (j % 8 == 0)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine("\n");
+
+                    Console.WriteLine("Number of person :" + count);
+                    Console.WriteLine("FirstName: " + contact[j]);
+                    Console.WriteLine("LastName: " + contact[j + 1]);
+                    Console.WriteLine("Address : " + contact[j + 2]);
+                    Console.WriteLine("City: " + contact[j + 3]);
+                    Console.WriteLine("State: " + contact[j + 4]);
+                    Console.WriteLine("Email: " + contact[j + 5]);
+                    Console.WriteLine("ZipCode: " + contact[j + 6]);
+                    Console.WriteLine("Phone Number: " + contact[j + 7]);
+                    count++;
+                }
+                j++;
+
+            }
+
+
+        }
+    }
+}
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -5,8 +5,102 @@ namespace AddressBook
     {
         public static void Main(string[] args)
         {
-            ContactDetails contactDetails = new ContactDetails();
-            contactDetails.AddMultipleContact();
+            while (true)
+            {
+
+                Console.WriteLine("WelCome To The Address Book");
+                Console.WriteLine("Select The Option For The Particular Address Book");
+                Console.WriteLine("1 for the Friends.");
+                Console.WriteLine("2 for the Office.");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                Program details = new Program();
+                switch (option)
+                {
+
+                    case 1:
+
+                        Friends friend = new Friends();
+
+                        do
+                        {
+                            Console.WriteLine("\n1:To Add a Friends Contact");
+                            Console.WriteLine("2:To Edit Friends Contacts");
+                            Console.WriteLine("3:To Remove a Friends contact");
+                            Console.WriteLine("4:To View Friends contact");
+                            {
+                                int option1 = Convert.ToInt32(Console.ReadLine());
+
+                                {
+                                    switch (option1)
+                                    {
+
+                                        case 1:
+                                            friend.AddingContacts();
+                                            continue;
+                                        case 2:
+                                            friend.EditContact();
+                                            break;
+                                        case 3:
+                                            friend.DeleteContact();
+                                            break;
+                                        case 4:
+                                            friend.FriendsView();
+                                            break;
+                                        default:
+                                            Console.WriteLine("Enter a valid Input");
+                                            break;
+                                    }
+                                    break;
+                                }
+                            }
+
+                        }
+
+                        while (option != 0);
+                        break;
+
+                    case 2:
+
+                        Office office = new Office();
+
+                        do
+                        {
+                            Console.WriteLine("\n1:To Add a Office Contact");
+                            Console.WriteLine("2:To Edit Office Contacts");
+                            Console.WriteLine("3:To Remove a Office contact");
+                            Console.WriteLine("4:To View Office contact");
+                            {
+                                int option1 = Convert.ToInt32(Console.ReadLine());
+                                {
+                                    switch (option1)
+                                    {
+
+                                        case 1:
+                                            office.AddingContacts();
+                                            continue;
+                                        case 2:
+                                            office.EditContact();
+                                            break;
+                                        case 3:
+                                            office.DeleteContact();
+                                            break;
+                                        case 4:
+                                            office.OfficeView();
+                                            break;
+                                        default:
+                                            Console.WriteLine("Enter a valid Input");
+                                            break;
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                        while (option != 0);
+                        break;
+                }
+
+            }
         }
 
     }
